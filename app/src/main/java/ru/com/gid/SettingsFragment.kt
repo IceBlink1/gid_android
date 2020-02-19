4dd484e56209c1a03eab12586670155b3b52786b
Alexander Lyutikov <lyutiko.alex@gmail.com>
2020-02-20 00:06:04 +0300

    Implemented huge part of settings

--- app/src/main/java/ru/com/gid/SettingsFragment.kt
+++ app/src/main/java/ru/com/gid/SettingsFragment.kt
@@ -2,10 +2,9 @@ package ru.com.gid
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.Preference
-import androidx.preference.PreferenceScreen
+
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.activity_settings, rootKey)
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
     }
 }
