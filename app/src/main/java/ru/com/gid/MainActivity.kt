d0ddc39a881718392c1d0b1a9f8c87a895004a89
Alexander Lyutikov <lyutiko.alex@gmail.com>
2020-02-24 17:01:30 +0300

    Yet contains bug

--- app/src/main/java/ru/com/gid/MainActivity.kt
+++ app/src/main/java/ru/com/gid/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.com.gid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,
-            SettingsFragment()).commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.nav_host_fragment_container, Fragment())
+            .replace(R.id.nav_host_fragment_container, SettingsFragment())
+            .commit()
         setContentView(R.layout.template_main)
 
     }
