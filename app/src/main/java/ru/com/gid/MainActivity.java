76918df882d019ee9b2e8a5e12329fea76f3cce0
Alexander Lyutikov <lyutiko.alex@gmail.com>
2020-03-02 11:07:15 +0300

    Rewrote kt code in java, added mvvm for profile

---
+++ app/src/main/java/ru/com/gid/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.com.gid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, SettingsActivity.class));
+    }
+}
---
+++ app/src/main/java/ru/com/gid/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package ru.com.gid;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
